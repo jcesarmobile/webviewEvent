bb6e86fc883346d544d454936dec8c1f43cf89fc
jcesarmobile <jcesarmobile@gmail.com>
2019-03-08 19:10:45 +0100

    Empty android

--- android/app/src/main/java/es/test/events/MainActivity.java
+++ android/app/src/main/java/es/test/events/MainActivity.java
@@ -1,12 +1,9 @@
 package es.test.events;
 
 import android.os.Bundle;
-import android.webkit.WebView;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-import com.mparticle.MParticle;
-import com.mparticle.MParticleOptions;
 
 import java.util.ArrayList;
 
@@ -20,10 +17,5 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
-    MParticleOptions options = MParticleOptions.builder(this)
-            .credentials("REPLACE ME WITH KEY", "REPLACE ME WITH SECRET")
-            .build();
-    MParticle.start(options);
-    MParticle.getInstance().registerWebView(this.bridge.getWebView());
   }
 }
